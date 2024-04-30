eec122c35d3091b819e22e4f9cbe39be93efb092
simochee <lollipop@simochee.net>
2024-04-30 23:44:41 +0900

    style(biome): format

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { routeTree } from './routeTree.gen'
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { createRoot } from "react-dom/client";
+import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router;
-  }
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 if (rootElement instanceof HTMLElement) {
-  createRoot(rootElement).render(<RouterProvider router={router} />);
+	createRoot(rootElement).render(<RouterProvider router={router} />);
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,16 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router"
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 const RootComponent = () => {
-  return (
-    <div>
-      <p>Hello World</p>
-      <Outlet />
-      <TanStackRouterDevtools />
-    </div>
-  )
-}
+	return (
+		<div>
+			<p>Hello World</p>
+			<Outlet />
+			<TanStackRouterDevtools />
+		</div>
+	);
+};
 
 export const Route = createRootRoute({
-  component: RootComponent
-})
+	component: RootComponent,
+});
